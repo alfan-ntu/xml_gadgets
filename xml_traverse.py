88bc14ea39832f9e42127ce58c793d71f5951462
alfan-ntu <maoyi_fan@yahoo.com.tw>
2023-11-12 22:45:43 +0800

    Added search by tag and debug dump utility features

--- xml_traverse.py
+++ xml_traverse.py
@@ -4,9 +4,9 @@
                 2) search a certain element
                 3) update a specific element with input value
                  of the input xml file
-    Date: 2023/11/9
+    Date: 2023/11/12
     Author:
-    Version: 0.1a
+    Version: 0.1b
     Revision History:
         - 2023/11/9: v. 0.1a the initial version
     Reference:
@@ -17,8 +17,38 @@
         - Add search and replace function
         - Convert the implementation to a class implementation to create a gadget set
 """
+import os.path
 import xml.etree.ElementTree as ET
 import pdb
+import inspect
+
+DIRECT_CALL = 1
+CALL_VIA_DUMP_DEBUG = 2
+
+
+def get_caller_info(call_depth=DIRECT_CALL):
+    """
+    Return the caller's file name and line number
+
+    @input call_depth: determine if the caller is from 'dump_debug_info(str)' or directly from the caller
+    @return filename, linenumber
+    """
+    caller_frame = inspect.stack()[call_depth]
+    caller_filename = caller_frame.filename
+    caller_filename = os.path.basename(caller_filename)
+    # assuming caller call this function immediately before using the file name and line number info.
+    caller_lineno = caller_frame.lineno+1 if call_depth==DIRECT_CALL else caller_frame.lineno
+    return caller_filename, caller_lineno
+
+
+def dump_debug_info(str):
+    """
+    Dump debug information with caller filename and caller linenumber
+
+    @input str: dump information
+    """
+    fn, lno = get_caller_info(CALL_VIA_DUMP_DEBUG)
+    print(f'{fn}:{lno} {str}')
 
 
 def tab_combo(level):
@@ -50,7 +80,10 @@ def breakdown_dict(dict):
 
 
 def traverse_tree(current, level):
-    """ Traverse the xml tree from input node, current, and record the level of this traverse """
+    """ Traverse the xml tree from input node, current, and record the level of this traverse
+    @param current: current node name
+    @param level: sub-level count
+    """
     tabc = tab_combo(level)
     if len(current.attrib) == 0:
         if just_cr(current.text):
@@ -65,8 +98,53 @@ def traverse_tree(current, level):
         traverse_tree(child, level)
 
 
+def search_element(node, tag, attrib=None, attrib_value=None):
+    """ Find a tag under a specified node and return the value
+
+    @param node: specify the current node for this search
+    @param tag
+    @param attrib
+    @param attrib_value
+    @return qualified node
+    """
+    good_node = None
+    for child in node.findall(tag):
+        if len(child.attrib) > 0:
+            # print(f'{tag} attribute has {len(child.attrib)} attributes: {child.attrib}')
+            if attrib is not None:
+                dump_debug_info(f'Node {node.tag} {attrib} value= {child.attrib[attrib]}')
+                if child.attrib[attrib] == attrib_value:
+                    fn, lno = get_caller_info()
+                    print(f'{fn}:{lno} Target {child.tag} node is found...')
+                    good_node = child
+
+    return good_node
+
+
 if __name__ == "__main__":
+    # For experimental purpose only
+    fn, lno = get_caller_info()
+    print(f'current file name: {fn}, current line number: {lno}')
+
     tree = ET.parse('./xml_collections/country_data.xml')
     root = tree.getroot()
 
-    traverse_tree(root, 0)
+    # traverse_tree(root, 0)
+    search_tag = 'neighbor'
+    search_attrib = 'name'
+    attrib_value = 'Malaysia'
+    if search_element(root, search_tag, search_attrib, attrib_value) is not None:
+        dump_debug_info(f'Tag {search_tag}: attrib:{search_attrib}={attrib_value} is found')
+    else:
+        dump_debug_info(f'Found no tag named: {search_tag}')
+
+    for child in root:
+        search_tag = 'neighbor'
+        search_attrib = 'name'
+        attrib_value = 'Malaysia'
+        # Go one level deeper
+        r_node = search_element(child, search_tag, search_attrib, attrib_value)
+        if r_node is not None:
+            print(f'Node identified: {r_node}')
+            dump_debug_info(f'Target neighbor country {attrib_value} is found!')
+            dump_debug_info(f'It\'s hostility status is: {r_node.find("hostility").text}')
